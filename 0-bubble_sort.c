a48d65a181213dce71b55a674a3a766025cf2018
bouzagui <badrbouzagui@gmail.com>
2024-04-28 23:32:04 +0100

    Done ✅

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,10 +13,10 @@ void bubble_sort(int *array, size_t size)
 	int tmp;
 	size_t i, j;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
